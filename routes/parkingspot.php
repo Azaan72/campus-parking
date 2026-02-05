31aed2cde306cfa33b336faff9629eff472b16b2
Ayaz Yerden <198673@novacollege.nl>
2026-02-05 15:30:02 +0100

    Reorganize parkingspot routes for improved readability and consistency

--- routes/parkingspot.php
+++ routes/parkingspot.php
@@ -3,24 +3,13 @@
 use App\Http\Controllers\ParkingSpotController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {});
 
 
-});
-
-//index
 Route::get('/parkingspots', [ParkingSpotController::class, 'index'])->name('parkingspots.index');
-// Show
-Route::get('/parkingspots/{parkingSpot}', [ParkingSpotController::class, 'show'])->name('parking-spots.show');
-    // Create
-    Route::get('/parkingspots/create', [ParkingSpotController::class, 'create'])->name('parkingspots.create');
-    Route::post('/parkingspots', [ParkingSpotController::class, 'store'])->name('parkingspots.store');
-
-
-
-    // Edit / Update
-    Route::get('/parkingspots/{parkingSpot}/edit', [ParkingSpotController::class, 'edit'])->name('parkingspots.edit');
-    Route::put('/parkingspots/{parkingSpot}', [ParkingSpotController::class, 'update'])->name('parkingspots.update');
-
-    // Delete
-    Route::delete('/parkingspots/{parkingSpot}', [ParkingSpotController::class, 'destroy'])->name('parkingspots.destroy');
+Route::get('/parkingspots/create', [ParkingSpotController::class, 'create'])->name('parkingspots.create');
+Route::post('/parkingspots', [ParkingSpotController::class, 'store'])->name('parkingspots.store');
+Route::get('/parkingspots/{parkingspot}', [ParkingspotController::class, 'show'])->name('parkingspots.show'); 
+Route::get('/parkingspots/{parkingspot}/edit', [ParkingSpotController::class, 'edit'])->name('parkingspots.edit');
+Route::put('/parkingspots/{parkingspot}', [ParkingSpotController::class, 'update'])->name('parkingspots.update');
+Route::delete('/parkingspots/{parkingspot}', [ParkingSpotController::class, 'destroy'])->name('parkingspots.destroy');
