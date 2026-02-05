cd5176d3922c43e7879994aaaf3c9c7c8813da7e
Ayaz Yerden <198673@novacollege.nl>
2026-02-05 13:51:38 +0100

    Refactor ParkingspotController to improve CRUD functionality and fix view references

--- app/Http/Controllers/ParkingspotController.php
+++ app/Http/Controllers/ParkingspotController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ParkingSpotStoreRequest;
+use App\Http\Requests\ParkingSpotUpdateRequest;
 use App\Models\Parkingspot;
 use Illuminate\Http\Request;
 
@@ -10,14 +12,53 @@ class ParkingspotController extends Controller
     public function index()
     {
         $title = 'Parking Spots';
-        $parkingspot = Parkingspot::all();
+        $parkingspots = Parkingspot::all();
         return view('parkingspots.index', compact('title', 'parkingspots'));
     }
 
     public function show($id)
     {
-        $parkingspot = Parkingspot::findOrFail($id);
+        $parkingspots = Parkingspot::findOrFail($id);
 
-        return view('parkingspot.show', compact('parkingspot'));
+        return view('parkingspots.show', compact('parkingspots'));
+    }
+
+    public function create()
+    {
+        return view('parkingspots.create');
+    }
+
+    public function store(ParkingSpotStoreRequest $request, Parkingspot $parkingspots)
+    {
+        $parkingspots = new Parkingspot();
+        $parkingspots->parkingspot_name = $request->input('parkingspot_name');
+        $parkingspots->save();
+
+        return redirect()->route('parkingspots.index');
+    }
+
+    public function update(ParkingSpotUpdateRequest $request, Parkingspot $parkingspots)
+    {
+
+        $parkingspots->parkingspot_name = $request->input('parkingspot_name');
+        $parkingspots->save();
+
+        return redirect()->route('parkingspots.show', ['parkingspots' => $parkingspots->id]);
+    }
+
+    public function edit($id)
+    {
+        $parkingspots = Parkingspot::findOrFail($id);
+
+        return view('parkingspots.edit')->with('status', 'Parkingspot succesvol bijgewerkt.')->with('parkingspots', $parkingspots);
+    }
+
+    public function destroy($id)
+    {
+        $parkingspots = Parkingspot::findOrFail($id);
+
+        $parkingspots->delete();
+
+        return redirect()->route('parkingspots.index');
     }
 }
