9fdd144e386e274892687894c86b49328730da35
Ayaz Yerden <198673@novacollege.nl>
2026-02-09 11:32:28 +0100

    role toevoegen aan de users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('zipcode');
             $table->string('country');
             $table->string('phone_number');
+            $table->string('role')->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
