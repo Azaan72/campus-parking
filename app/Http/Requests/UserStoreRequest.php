abd4ec238b8aa80ead287fcf80a52d222cd4f785
Ayaz Yerden <198673@novacollege.nl>
2026-02-09 10:56:24 +0100

    form requests maken voor users

---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'prefix' => 'nullable|string|max:50',
+            'role' => 'required|string|in:admin,user',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'phone_number' => 'nullable|string|max:20',
+            'streetname' => 'nullable|string|max:255',
+            'house_number' => 'nullable|string|max:10',
+            'zipcode' => 'nullable|string|max:20',
+            'city' => 'nullable|string|max:255',
+            'country' => 'nullable|string|max:255',
+        ];
+    }
+}
