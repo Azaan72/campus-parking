7ed2962a88e570dc59745d62d014a9f7a39edfcd
Ayaz Yerden <198673@novacollege.nl>
2026-03-03 11:40:01 +0100

    Refactor LocationController to improve variable naming and streamline location handling

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -14,10 +14,12 @@ class LocationController extends Controller
         $title = 'Parking locations';
         $type = $request->get('type');
 
-        $location = Location::when($type, fn($q) => $q->byType($type))->get();
+        // Fetch all locations (or filter by type if needed)
+        $locations = Location::when($type, fn($q) => $q->byType($type))->get();
 
-        return view('locations.index', compact('title', 'location', 'type'));
-        }
+        // Pass $locations to the view (not $location)
+        return view('locations.index', compact('title', 'locations', 'type'));
+    }
 
     public function show(Location $location)
     {
@@ -34,9 +36,9 @@ class LocationController extends Controller
     public function store(LocationStoreRequest $request)
     {
         $location = Location::create([
-            'location_name' => $request->input('location_name'),
+            'location' => $request->input('location'),
         ]);
-    
+
 
         return redirect()->route('locations.index')
             ->with('success', 'Location succesvol aangemaakt.');
@@ -46,7 +48,7 @@ class LocationController extends Controller
     public function update(LocationUpdateRequest $request, Location $location)
     {
         $location->update([
-            'location_name' => $request->input('location_name'),
+            'location' => $request->input('location'),
         ]);
 
 
@@ -60,12 +62,11 @@ class LocationController extends Controller
         return view('locations.edit')->with('status', 'Location succesvol bijgewerkt.')->with('location', $location);
     }
 
-    public function destroy($id)
+    public function destroy(Location $location)
     {
-        $location = Location::findOrFail($id);
-
         $location->delete();
 
-        return redirect()->route('locations.index');
+        return redirect()->route('locations.index')
+            ->with('success', 'Location succesvol verwijderd.');
     }
 }
