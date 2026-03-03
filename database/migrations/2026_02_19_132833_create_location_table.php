fa7b15e2c9ca5b795f83d91544e3956734d0c5c2
Ayaz Yerden <198673@novacollege.nl>
2026-03-03 11:13:19 +0100

    Add location_name field to location migration

--- database/migrations/2026_02_19_132833_create_location_table.php
+++ database/migrations/2026_02_19_132833_create_location_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('location', function (Blueprint $table) {
             $table->id();
+            $table->string('location_name');
             $table->timestamps();
         });
     }
