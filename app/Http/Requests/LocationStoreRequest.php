4c256b315a5fa2555572753d136672d1d94ce9ac
Ayaz Yerden <198673@novacollege.nl>
2026-03-03 11:12:02 +0100

    Fix authorization in LocationStoreRequest and LocationUpdateRequest to allow user requests

--- app/Http/Requests/LocationStoreRequest.php
+++ app/Http/Requests/LocationStoreRequest.php
@@ -11,7 +11,7 @@ class LocationStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class LocationStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'location_name' => 'required|string|max:255',
         ];
     }
 }
