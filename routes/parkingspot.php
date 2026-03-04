5a061110e0d61f79eb6aaec043193847af9394e4
Ayaz Yerden <198673@novacollege.nl>
2026-03-04 11:27:24 +0100

    Refactor route files to group authenticated routes and remove duplicate show route definitions

--- routes/parkingspot.php
+++ routes/parkingspot.php
@@ -3,13 +3,16 @@
 use App\Http\Controllers\ParkingSpotController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {});
+
 
 
 Route::get('/parkingspots', [ParkingSpotController::class, 'index'])->name('parkingspots.index');
+Route::get('/parkingspots/{parkingspot}', [ParkingspotController::class, 'show'])->name('parkingspots.show'); 
+
+Route::middleware('auth')->group(function () {
 Route::get('/parkingspots/create', [ParkingSpotController::class, 'create'])->name('parkingspots.create');
 Route::post('/parkingspots', [ParkingSpotController::class, 'store'])->name('parkingspots.store');
-Route::get('/parkingspots/{parkingspot}', [ParkingspotController::class, 'show'])->name('parkingspots.show'); 
 Route::get('/parkingspots/{parkingspot}/edit', [ParkingSpotController::class, 'edit'])->name('parkingspots.edit');
 Route::put('/parkingspots/{parkingspot}', [ParkingSpotController::class, 'update'])->name('parkingspots.update');
 Route::delete('/parkingspots/{parkingspot}', [ParkingSpotController::class, 'destroy'])->name('parkingspots.destroy');
+});
--- routes/reservation.php
+++ routes/reservation.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
+Route::get('/reservations/{reservation}', [ReservationController::class, 'show'])->name('reservations.show');
+
+Route::middleware('auth')->group(function () {
 Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
 Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
-Route::get('/reservations/{reservation}', [ReservationController::class, 'show'])->name('reservations.show');
 Route::get('/reservations/{reservation}/edit', [ReservationController::class, 'edit'])->name('reservations.edit');
 Route::put('/reservations/{reservation}', [ReservationController::class, 'update'])->name('reservations.update');
 Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
 Route::patch('reservations/{reservation}/cancel', [ReservationController::class, 'cancel'])
     ->name('reservations.cancel');
+});
